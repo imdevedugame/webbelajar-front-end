526fe072b04bf3d8b97943ba286dcab7133e2fde
naufaladrian <naufal.adrian904@gmail.com>
2024-11-16 13:28:40 +0700

    feat : import responsive navbar component

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,3 +1,10 @@
+import Navbar from "../components/Navbar";
+
 export default function Root() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 }
