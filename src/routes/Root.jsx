b118a0e1c3629b3bc24dbf7eb5d87950c7c74a90
naufaladrian <naufal.adrian904@gmail.com>
2024-11-17 10:56:22 +0700

    feat: import section

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,5 +1,7 @@
 import HeroSection from "../components/HeroSection";
 import Navbar from "../components/Navbar";
+import PortfolioSection from "../components/PortfolioSection";
+import TechSection from "../components/TechSection";
 
 export default function Root() {
   return (
@@ -7,6 +9,8 @@ export default function Root() {
       <Navbar />
       <main className="container mx-auto  px-4 sm:px-6 lg:px-8">
         <HeroSection />
+        <PortfolioSection />
+        <TechSection />
       </main>
     </>
   );
